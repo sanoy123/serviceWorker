0012cbaf1c6d7c64540422c738ed03065a97111b
sanoy123 <expertsanoy@gmail.com>
2018-01-18 11:32:36 -0500

    fixing api error

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
     .register('./sw.js', { scope: './' })
@@ -10,7 +10,7 @@
     })
 
 }
-*/
+
 
 function getStories(){
 
