5ef809228241ddb08a32f1d13375eb4d5ed11f75
sanoy123 <expertsanoy@gmail.com>
2018-01-18 11:25:28 -0500

    fixing api error

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
     .register('./sw.js', { scope: './' })
@@ -10,6 +10,7 @@ if ('serviceWorker' in navigator) {
     })
 
 }
+*/
 
 function getStories(){
 
@@ -21,13 +22,13 @@ function getStories(){
       document.getElementById('stories').innerHTML =
       `<div class="panel panel-default">
         <div class="panel-heading">
-         <h3 class="panel-title">` + stories['articles'].title + `</h3>
+         <h3 class="panel-title">` + stories.articles.title + `</h3>
         </div>
         <div class="panel-body">
           <div class="col-md-3">
-            <img src="`+stories['articles'].urlToImage+`">
+            <img src="`+stories.articles.urlToImage+`">
           </div>
-          <div class="col-md-9">`+stories['articles'].description+`</div>
+          <div class="col-md-9">`+stories.articles.description+`</div>
         </div>
       </div>`;
     }
